721fa2d5dadfbd4ed57c50eb69e5f19203ac9f63
sooje0ng <tnwjd7890@naver.com>
2023-10-14 19:15:37 +0900

    feat: 모달형식 바꾸기, select모달 ui 완성 #1

--- src/style/LandingPage.style.js
+++ src/style/LandingPage.style.js
@@ -1,13 +1,54 @@
 import styled from 'styled-components';
+import BackgroundImage from '../assets/image/background.png';
 
 export const Wrapper = styled.div`
   box-sizing: border-box;
-  width: 100vw;
-  height: 100vh;
-  /* background-color: black; */
-`;
-
-export const Background = styled.img`
   width: 100%;
   height: 100%;
+`;
+
+export const Background = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background: url(${BackgroundImage});
+  background-size: 100vw 100vh;
+  z-index: 0;
+`;
+
+export const Modal = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 380px;
+  height: 430px;
+  background: white;
+  opacity: 0.85;
+  border-radius: 5px;
+  outline: none;
+`;
+export const ModalWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 30px 0;
+`;
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: fit-content;
+  margin-top: 40px;
+  text-align: center;
+`;
+
+export const Logo = styled.img`
+  margin: 0 auto;
+  width: 230px;
+`;
+
+export const ContentContainer = styled.div`
+  width: 70%;
+  height: 70%;
+  margin: 8vh auto;
+  text-align: center;
 `;
