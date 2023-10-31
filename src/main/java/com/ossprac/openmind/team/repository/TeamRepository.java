2fd913d3c609e8bf7500f37d9140a3bd9d68a7a1
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-10-31 23:22:10 +0900

    [feat] Team repository 생성

---
+++ src/main/java/com/ossprac/openmind/team/repository/TeamRepository.java
@@ -0,0 +1,11 @@
+package com.ossprac.openmind.team.repository;
+
+import com.ossprac.openmind.team.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+}
+
