bbd1f9b9cd8df2fb5414d7b3cb7bab9bfa786a4b
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-05 17:45:15 +0900

    delete src

--- src/main/java/com/ossprac/openmind/lecture/repository/LectureRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ossprac.openmind.lecture.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ossprac.openmind.lecture.entity.Lecture;
-
-public interface LectureRepository extends JpaRepository<Lecture, Long> {
-}
--- src/main/java/com/ossprac/openmind/lecture/repository/LectureTimeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ossprac.openmind.lecture.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ossprac.openmind.lecture.entity.LectureTime;
-
-public interface LectureTimeRepository extends JpaRepository<LectureTime, Long> {
-}
