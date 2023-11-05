924d2d9b4dbc2d067e6693175cdc3363b1a16515
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-05 17:44:35 +0900

    delete src

--- src/pages/DocumentPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const DocumentPage = () => {
-  return <div>이곳은 팀 스페이스의 [회의록] 탭입니다.</div>;
-};
-
-export default DocumentPage;
--- src/pages/SchedulePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SchedulePage = () => {
-  return <div>이곳은 팀 스페이스의 [일정관리] 탭입니다.</div>;
-};
-
-export default SchedulePage;
