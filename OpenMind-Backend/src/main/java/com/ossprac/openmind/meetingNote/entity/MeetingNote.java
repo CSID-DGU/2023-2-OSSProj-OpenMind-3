6e569224bba7187a2611c4cd7eb16a6681271e36
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-12 22:19:09 +0900

    feat: MeetingNote entity 생성

---
+++ OpenMind-Backend/src/main/java/com/ossprac/openmind/meetingNote/entity/MeetingNote.java
@@ -0,0 +1,35 @@
+package com.ossprac.openmind.meetingNote.entity;
+
+import com.ossprac.openmind.team.entity.Team;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "meetingNotes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MeetingNote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "meeting_note_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    private String title;
+
+    private String content;
+
+    @Builder
+    public MeetingNote(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
