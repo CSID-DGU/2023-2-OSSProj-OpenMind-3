c58d87018773640cde0cd0b66b7b78d16334e791
sooje0ng <tnwjd7890@naver.com>
2023-11-13 11:26:21 +0900

    feat:회의록 목록 ui 완성 #13

--- src/pages/DocumentPage.jsx
+++ src/pages/DocumentPage.jsx
@@ -1,7 +1,63 @@
 import React from 'react';
+import * as s from '../style/DocumentPage.style';
+import { useParams } from 'react-router-dom';
+import plus from '../assets/icon/WhiteAdd.svg';
+import pen from '../assets/icon/pen.svg';
+import trash from '../assets/icon/trash.svg';
 
 const DocumentPage = () => {
-  return <div>이곳은 팀 스페이스의 [회의록] 탭입니다.</div>;
+  const params = useParams();
+  const teamId = Number(params.teamId.substring(1));
+
+  const handleClickAddButton = () => {};
+  return (
+    <s.Wrapper>
+      <s.DocumentListContainerHeader>
+        <s.HeaderLabel>회의록 목록</s.HeaderLabel>
+        <s.AddButton src={plus} onClick={handleClickAddButton}></s.AddButton>
+      </s.DocumentListContainerHeader>
+      <s.DocumentListContainer>
+        <div
+          className='table-responsive project-list'
+          style={{
+            height: '220px',
+            overflowY: 'auto',
+          }}
+        >
+          <table className='table project-table table-centered table-nowrap'>
+            <thead>
+              <tr>
+                <th scope='col'></th>
+                <th scope='col' style={{ textAlign: 'center' }}>
+                  제목
+                </th>
+                <th scope='col' style={{ textAlign: 'center' }}>
+                  수정일
+                </th>
+                <th scope='col' style={{ textAlign: 'center' }}>
+                  작성자
+                </th>
+                <th scope='col' style={{ width: '100px' }}></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr style={{ textAlign: 'center' }}>
+                <th scope='row'>1</th>
+                <td>제안서 PPT 제작</td>
+                <td>2023.11.13</td>
+                <td>테스트계정</td>
+                <td>
+                  <s.Icon src={pen} onClick={handleClickAddButton}></s.Icon>
+
+                  <s.Icon src={trash} onClick={handleClickAddButton}></s.Icon>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </s.DocumentListContainer>
+    </s.Wrapper>
+  );
 };
 
 export default DocumentPage;
---
+++ src/style/DocumentPage.style.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: white;
+`;
+
+export const DocumentListContainerHeader = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  width: 60vw;
+  height: 6vh;
+  margin-top: 30px;
+  border-bottom: transparent;
+  border-radius: 15px 15px 0 0;
+  background-color: #f59c00;
+  box-sizing: border-box;
+`;
+
+export const DocumentListContainer = styled.div`
+  width: 60vw;
+  height: 70vh;
+  border: 1px solid #b0b0b0;
+  border-radius: 0 0 15px 15px;
+  margin-top: 0;
+  box-sizing: border-box;
+`;
+export const HeaderLabel = styled.p`
+  width: 97%;
+  margin: 0;
+  padding: 0;
+  color: white;
+`;
+export const AddButton = styled.img`
+  width: 20px;
+  height: 20px;
+  z-index: 99;
+  cursor: pointer;
+`;
+
+export const Icon = styled.img`
+  margin-right: 15px;
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+`;
