516ecef772eb15a9c75f789a37c718b03e8080da
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-11-29 22:08:49 +0900

    fix: EventResponseDto 날짜 리턴 타입 변경

--- OpenMind-Backend/src/main/java/com/ossprac/openmind/calendar/dto/EventUpdateRequestDto.java
+++ OpenMind-Backend/src/main/java/com/ossprac/openmind/calendar/dto/EventUpdateRequestDto.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 public class EventUpdateRequestDto {
     private String title;
     private String description;
-    private String startDate;
-    private String endDate;
+    private String start;
+    private String end;
 }
