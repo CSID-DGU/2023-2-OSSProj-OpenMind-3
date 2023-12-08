b115297b8d2fe3a8d440e8808ea53f690c515a4b
JINU-CHANG <jenny010925@naver.com>
2023-12-09 02:22:45 +0900

    feat: 프론트 요청으로 LocalTime 반환 형식 변경

--- OpenMind-Backend/src/main/java/com/ossprac/openmind/team/dto/res/TeamScheduleResponse.java
+++ OpenMind-Backend/src/main/java/com/ossprac/openmind/team/dto/res/TeamScheduleResponse.java
@@ -4,16 +4,22 @@ import com.ossprac.openmind.lecture.entity.DaysOfWeek;
 import lombok.Getter;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 public class TeamScheduleResponse {
     private DaysOfWeek daysOfWeek;
-    private LocalTime startTime;
-    private LocalTime endTime;
+    private String startTime;
+    private String endTime;
 
     public TeamScheduleResponse(DaysOfWeek daysOfWeek, LocalTime startTime, LocalTime endTime) {
         this.daysOfWeek = daysOfWeek;
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.startTime = formatLocalTime(startTime);
+        this.endTime = formatLocalTime(endTime);
+    }
+
+    private String formatLocalTime(LocalTime localTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return localTime.format(formatter);
     }
 }
